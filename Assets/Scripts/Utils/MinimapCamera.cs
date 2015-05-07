6fedb9e3060be0f89fb1318c1501d68b1f4b0127
Xin Sun <xin.sun@st-hughs.ox.ac.uk>
2015-05-07 20:15:06 +0100

    moving camera

---
+++ Assets/Scripts/Utils/MinimapCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCamera : MonoBehaviour {
+
+	// Update is called once per frame
+	public Transform Target;
+	void Update () {
+		transform.position = new Vector3 (Target.position.x, transform.position.y, Target.position.z);
+	}
+}
